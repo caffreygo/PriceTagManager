28b82909084adb3545ddf8618f77ecdbb9521f10
caffrey <caffreygo@163.com>
2019-03-20 22:04:57 +0800

    edit and delete servlet

--- src/caffrey/servlets/delete.java
+++ src/caffrey/servlets/delete.java
@@ -2,26 +2,21 @@ package caffrey.servlets;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import caffrey.jdbc.DbUtil;
 
-import caffrey.dao.TitleDao;
-import caffrey.entity.Title;
-
-public class getList extends HttpServlet {
+public class delete extends HttpServlet {
 
 
 	private static final long serialVersionUID = 1L;
 
 
-	public getList() {
+	public delete() {
 		super();
 	}
 
@@ -34,39 +29,32 @@ public class getList extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+			this.doPost(request, response);
 
-		this.doPost(request, response);
 	}
 
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-        response.setContentType("text/html;charset=utf-8");
+		response.setContentType("text/html;charset=utf-8");
         /*设置响应头允许ajax跨域访问*/
         response.setHeader("Access-Control-Allow-Origin", "*");
-
         /* 星号表示所有的异域请求都可以接受， */
         response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+        //获取微信小程序get的参数值并打印
+        int goodsId=Integer.parseInt(request.getParameter("goodsId"));
+        DbUtil db=new DbUtil();
+ 		String sql="DELETE FROM goods WHERE goodsId="+goodsId;
+ 		Object[] objs=new Object[]{};
+		db.update(sql, objs);
+		
+		String json ="{\"success\":true,\"content\":\"删除成功！\"}";
+	    Writer out = response.getWriter();
+	    out.write(json);
+	    out.flush();
+	    out.close();
 
-        TitleDao dao=new TitleDao();
-        List<Title> list =dao.readFirstTitle();   
-        JSONArray json = new JSONArray();
-        for(Title tl:list){
-        	
-			 JSONObject jo = new JSONObject();
-			 
-		     jo.put("goodsId", tl.getGoodsId());
-		     jo.put("goodsName", tl.getGoodsName());
-		     jo.put("goodsPrice", tl.getGoodsPrice());
-		     jo.put("goodsInfo", tl.getGoodsInfo());
-		      
-		     json.put(jo);
-        }
-        Writer out = response.getWriter();
-        out.write(json.toString());
-        out.flush();
-        out.close();
 	}
 
 
--- src/caffrey/servlets/edit.java
+++ src/caffrey/servlets/edit.java
@@ -1,13 +1,15 @@
 package caffrey.servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Writer;
 //import java.io.Writer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import caffrey.jdbc.DbUtil;
 
 //import caffrey.jdbc.DbUtil;
 
@@ -30,45 +32,47 @@ public class edit extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		response.setContentType("text/html;charset=utf-8");
         /*设置响应头允许ajax跨域访问*/
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setCharacterEncoding("UTF-8");
+        /* 解决中文乱码 */
+        request.setCharacterEncoding("UTF-8");
         /* 星号表示所有的异域请求都可以接受， */
         response.setHeader("Access-Control-Allow-Methods", "GET,POST");
-        
-        String goodsId = request.getParameter("goodsId");
-//        String goodsPrice = request.getParameter("goodsPrice");
-//        String goodsInfo = request.getParameter("goodsInfo");
-//        String goodsInfo =new String(request.getParameter("goodsInfo").getBytes("ISO8859-1"),"UTF-8");
-        System.out.println(goodsId);
-//        if( !goodsName.equals("") && !goodsPrice.equals("")){
-//        	DbUtil db=new DbUtil();
-//    		String sql="UPDATE goods SET goodsName= ? WHERE goodsId= ?";
-//    		Object[] objs1=new Object[]{goodsName, goodsId};
-//    		db.update(sql, objs1);
-//    		
-//    		sql="UPDATE goods SET goodsPrice= ? WHERE goodsId= ? ";
-//    		Object[] objs2=new Object[]{goodsPrice, goodsId};
-//    		db.update(sql, objs2);
-//    		
-//    		sql="UPDATE goods SET goodsInfo= ? WHERE goodsId= ? ";
-//    		Object[] objs3=new Object[]{goodsInfo, goodsId};
-//    		db.update(sql, objs3);
-//    		
-//    		String json ="{\"success\":true,\"content\":\"编辑成功！\"}";
-//  	        Writer out = response.getWriter();
-//  	        out.write(json);
-//  	        out.flush();
-//  	        out.close();
-//    
-//        }else{
-//        	String json ="{\"success\":false,\"content\":\"编辑失败！\"}";
-//  	        Writer out = response.getWriter();
-//  	        out.write(json);
-//  	        out.flush();
-//  	        out.close();
-//        }
+        int goodsId=Integer.parseInt(request.getParameter("goodsId"));
+        String goodsName = request.getParameter("goodsName");
+        String goodsPrice = request.getParameter("goodsPrice");
+        String goodsInfo = request.getParameter("goodsInfo");
+   
+        if( !goodsName.equals("") && !goodsPrice.equals("")){
+        	DbUtil db=new DbUtil();
+    		String sql="UPDATE goods SET goodsName= ? WHERE goodsId= ?";
+    		Object[] objs1=new Object[]{goodsName, goodsId};
+    		db.update(sql, objs1);
+    		
+    		sql="UPDATE goods SET goodsPrice= ? WHERE goodsId= ? ";
+    		Object[] objs2=new Object[]{goodsPrice, goodsId};
+    		db.update(sql, objs2);
+    		
+    		sql="UPDATE goods SET goodsInfo= ? WHERE goodsId= ? ";
+    		Object[] objs3=new Object[]{goodsInfo, goodsId};
+    		db.update(sql, objs3);
+    		
+    		String json ="{\"success\":true,\"content\":\"编辑成功！\"}";
+  	        Writer out = response.getWriter();
+  	        out.write(json);
+  	        out.flush();
+  	        out.close();
+    
+        }else{
+        	String json ="{\"success\":false,\"content\":\"编辑失败！\"}";
+  	        Writer out = response.getWriter();
+  	        out.write(json);
+  	        out.flush();
+  	        out.close();
+        }
 		
 	}
 
